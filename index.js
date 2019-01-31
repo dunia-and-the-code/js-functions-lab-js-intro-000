47bb3fa19ac36bab4c83b1f0d8b085f77792bba6
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-31 13:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
-happyHolidaysTo`${you}`;
+happyHolidaysTo('you');
 
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
