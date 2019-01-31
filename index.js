975b5b502053e0661426da303aaed684b80594d6
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-31 13:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function happyHolidaysTo(holiday, name) {
 holiday("Independence Day");
 name("you");
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
 days(20);
